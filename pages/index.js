8ae9c4e6a81bb4fb7d1a5b33d68d7adc4fa6eb17
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-09 00:59:08 +0100

    refactor: code a new document and my document section in pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,41 @@ export default function Home() {
         <title>Google_docs_clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <Header/>
-      
+      <Header />
+      <section className="bg-[#fbf9fa] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className=" flex items-center justify-between py-6 ">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+            <Button
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="3xl" />
+            </Button>
+          </div>
+          <div>
+            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
+              <Image src="https://links.papareact.com/pju " layout="fill" />
+            </div>
+
+            <p className="ml-2 mt-2  font-semibold text-sm text-gray-700">
+              Blank
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="bg-white px-10 md:px-0">
+        <div className="max-w-3xl mx-auto py-8 text-gray-700">
+          <div className="flex items-center justify-between pb-5">
+            <h2 className="font-medium flex-grow"> My documents</h2>
+            <p className="mr-12">Date created </p>
+            <Icon name="folder" size="3xl" color="gray" />
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
- 
