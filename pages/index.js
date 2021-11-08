8eed52a7510605f7073dc468d8062e4dadf742d5
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-09 00:57:21 +0100

    refactor: import a Button,image and icon in pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Header from "../components/Header";
+import Icon from "@material-tailwind/react/Icon";
+import Button from "@material-tailwind/react/Button";
+import Image from "next/image";
 export default function Home() {
   return (
     <div>
